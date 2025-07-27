c7a57e029f3ea5ae8d9ffe4150d34209653eec8b
m0v1 <mail@atomoperate.com>
2025-07-27 22:45:22 +0800

    feat: init project

---
+++ webapp-validation/src/main/java/org/example/webapp/validation/WebappValidationApplication.java
@@ -0,0 +1,13 @@
+package org.example.webapp.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebappValidationApplication.class, args);
+    }
+
+}
---
+++ webapp-validation/src/test/java/org/example/webapp/validation/WebappValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.webapp.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
